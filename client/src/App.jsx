a8e58296426b6a8cf3e01044a2a075c57a8c8403
chenxi-debugger <chenxi.debugger@gmail.com>
2025-04-21 13:39:53 -0700

    Renamed project to analytics_dashboard_fullstack

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 
 const App = () => {
-  return <Dashboard />;
+  return (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      {/* Add more routes here as needed */}
+    </Routes>
+  </Router>
+  );
 };
 
 export default App;
