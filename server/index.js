3061d4fed23e7c6bda346c3ebacc10d1a4bbfefc
chenxi-debugger <chenxi.debugger@gmail.com>
2025-04-22 10:00:27 -0700

    Change name and change the index.js into import way not require way

--- server/index.js
+++ server/index.js
@@ -1,22 +1,37 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const mysql = require('mysql2/promise');
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import mysql from 'mysql2/promise';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
+
 app.get('/api/stats', async (req, res) => {
   res.json({ visits: 1000, sales: 500 });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
 });
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
-  app.listen(5001, () => {
-    console.log('Server running on port 5001');
-  });
-  
+app.get('/', (req, res) => {
+  res.send('Welcome to the Analytics Dashboard API');
+});
+
+app.listen(5001, () => {
+  console.log('Server running on port 5001');
+});
+
