f905cb028d783f11b82fcc0bf4b7c2ac6abc82da
chenxi-debugger <chenxi.debugger@gmail.com>
2025-07-24 13:08:36 -0700

    addtheme

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// import ThemeContextProvider from './theme/themeContext';
+import ThemeContextProvider from './theme/themeContext';
 // import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 // import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import './styles/index.css';
@@ -12,9 +12,9 @@ import './styles/index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <QueryClientProvider client={queryClient}> */}
-      {/* <ThemeContextProvider> */}
+      <ThemeContextProvider>
         <App />
-      {/* </ThemeContextProvider> */}
+      </ThemeContextProvider>
       {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     {/* </QueryClientProvider> */}
   </React.StrictMode>
