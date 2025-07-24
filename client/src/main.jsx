8cb4fc4fc2496dcc20bddade1800e7d178ed041c
chenxi-debugger <chenxi.debugger@gmail.com>
2025-07-24 10:49:50 -0700

    🧹 Remove unused ReactQueryDevtools and ThemeContext

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App';
-import ThemeContextProvider from './theme/themeContext';
+// import ThemeContextProvider from './theme/themeContext'; ❌ 暂时注释
 import './styles/index.css';
-
-// Create a QueryClient instance
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ThemeContextProvider>
-        <App />
-      </ThemeContextProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    {/* <ThemeContextProvider> */}
+      <App />
+    {/* </ThemeContextProvider> */}
   </React.StrictMode>
 );
