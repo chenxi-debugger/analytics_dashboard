a5f3abd9c8a356e0429cd0898da2a9fa631b25de
chenxi-debugger <chenxi.debugger@gmail.com>
2025-07-24 12:36:32 -0700

    Fix Babel configuration for emotion plugin in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,18 +2,22 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react({
-    babel: {
-      plugins: [
-        '@emotion/babel-plugin',
-        { sourceMap: true, autoLabel: 'dev-only' }
-      ],
-    },
-  })],
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          ['@emotion/babel-plugin', {
+            sourceMap: true,
+            autoLabel: 'dev-only'
+          }]
+        ],
+      },
+    }),
+  ],
   build: {
     rollupOptions: {
       output: {
-        manualChunks: undefined // 暂时禁用以隔离问题
+        manualChunks: undefined // 保持禁用以隔离问题
       }
     }
   },
